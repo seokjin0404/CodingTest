1526bf49825dfce6e53f19a31f9457bb687c24c1
TSTP <86605720+seokjin0404@users.noreply.github.com>
2022-04-18 22:13:58 +0900

    upload 04-18

--- 백준/알고리즘기초1／2/practice.py
+++ 백준/알고리즘기초1／2/practice.py
@@ -1,4 +1,8 @@
-N, M = map(int,input().split())
-
-array = [int(input()) for _ in range(N)]
-d = [10001] *(M+1)
+def dfs(elements, start, k):
+    if k == 0:
+        results.append(elements[:])
+        return
+    for i in range(start, n+1):
+        elements.append(i)
+        dfs(elements, i+1, k-1)
+        elements.pop()
---
+++ 백준/알고리즘기초2／2/2022-04-18-15649.py
@@ -0,0 +1,12 @@
+N, M = map(int,input().split())
+result = []
+
+def dfs():
+    if len(result) == M:
+        print(' '.join(map(str,result)))
+    for i in range(1, N+1):
+        if i not in result:
+            result.append(i)
+            dfs()
+            result.pop()
+dfs()
